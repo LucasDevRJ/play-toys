8e28196294d81b4a28ae566d1e75fe95c60f3db4
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-12-27 20:48:05 -0300

    Método para cadastrar brinquedo.

--- api/src/main/java/play/toys/api/controller/BrinquedoController.java
+++ api/src/main/java/play/toys/api/controller/BrinquedoController.java
@@ -1,4 +1,17 @@
 package play.toys.api.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import play.toys.api.brinquedo.DadosDoBrinquedo;
+
+@RestController
+@RequestMapping("brinquedos")
 public class BrinquedoController {
+
+    @PostMapping
+    public void cadastrarBrinquedo(@RequestBody DadosDoBrinquedo dados) {
+        System.out.println(dados);
+    }
 }
