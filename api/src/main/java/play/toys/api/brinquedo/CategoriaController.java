85512af3898afc7ab8398b2e47ee80843d2620a9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-12-28 19:33:11 -0300

    Criação da classe Categoria junto com suas notações do Spring Boot.

---
+++ api/src/main/java/play/toys/api/brinquedo/CategoriaController.java
@@ -0,0 +1,9 @@
+package play.toys.api.brinquedo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("categorias")
+public class CategoriaController {
+}
