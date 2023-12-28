362f647ed73fbfc6081d2e17527c728de462ff93
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-12-28 19:34:55 -0300

    Método para adicionar Categoria

--- api/src/main/java/play/toys/api/brinquedo/CategoriaController.java
+++ api/src/main/java/play/toys/api/brinquedo/CategoriaController.java
@@ -1,9 +1,16 @@
 package play.toys.api.brinquedo;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("categorias")
 public class CategoriaController {
+
+    @PostMapping
+    public void cadastrarCategoria(@RequestBody DadosDaCategoria dados) {
+        System.out.println(dados);
+    }
 }
