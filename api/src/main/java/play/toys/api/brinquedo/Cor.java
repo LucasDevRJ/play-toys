04d2e95dab6be2a48c7aaecbe19f8beee7fae3fd
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-12-28 21:26:50 -0300

    Anotações JPA

--- api/src/main/java/play/toys/api/brinquedo/Cor.java
+++ api/src/main/java/play/toys/api/brinquedo/Cor.java
@@ -1,14 +1,22 @@
 package play.toys.api.brinquedo;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "cores")
+@Entity(name = "Cor")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
 public class Cor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String cor;
-
-    public String getCor() {
-        return cor;
-    }
-
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
+    private String nome;
 }
