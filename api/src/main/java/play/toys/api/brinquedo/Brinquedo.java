273940b7efc394fce8bd323fbb99eab23dc7771d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-12-28 21:06:13 -0300

    Criação da classe Brinquedo.

---
+++ api/src/main/java/play/toys/api/brinquedo/Brinquedo.java
@@ -0,0 +1,15 @@
+package play.toys.api.brinquedo;
+
+import java.math.BigDecimal;
+
+public class Brinquedo {
+    private Long id;
+    private String nome;
+    private BigDecimal preco;
+    private String descricao;
+    private String modelo;
+    private Marca marca;
+    private Categoria categoria;
+    private Cor cor;
+    private FaixaEtaria faixaEtaria;
+}
