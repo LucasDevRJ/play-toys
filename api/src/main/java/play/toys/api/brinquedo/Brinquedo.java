aefcd8a54ad8d2c2d0b7d6ee8b4083b4412b7de7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-12-28 21:13:07 -0300

    Relacionamentos na classe

--- api/src/main/java/play/toys/api/brinquedo/Brinquedo.java
+++ api/src/main/java/play/toys/api/brinquedo/Brinquedo.java
@@ -1,15 +1,25 @@
 package play.toys.api.brinquedo;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Table(name = "brinquedos")
+@Entity(name = "Brinquedo")
 public class Brinquedo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private BigDecimal preco;
     private String descricao;
     private String modelo;
+    @OneToOne
     private Marca marca;
+    @OneToOne
     private Categoria categoria;
+    @OneToMany
     private Cor cor;
+    @OneToOne
     private FaixaEtaria faixaEtaria;
 }
