5b374409725c2d64ea7907fd4899fb3517929be6
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-12-28 21:17:42 -0300

    Anotações JPA

--- api/src/main/java/play/toys/api/brinquedo/Brinquedo.java
+++ api/src/main/java/play/toys/api/brinquedo/Brinquedo.java
@@ -1,11 +1,19 @@
 package play.toys.api.brinquedo;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Table(name = "brinquedos")
 @Entity(name = "Brinquedo")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Brinquedo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
